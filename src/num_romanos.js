aa5ec9594ba8a83c66c63ce3726f23149cf3c100
Jesus Ariel Mamani Gutierrez <ariel.10.mg2000@gmail.com>
2025-02-24 22:06:53 -0400

    add: funcion modificada para que el numero 10 funcione

--- src/num_romanos.js
+++ src/num_romanos.js
@@ -1,5 +1,6 @@
 function convertirARomano(numero) {
     const valores = [
+      { valor: 10, simbolo: 'X' },
       { valor: 9, simbolo: 'IX' },
       { valor: 5, simbolo: 'V' },
       { valor: 4, simbolo: 'IV' },
