1e3efd67e672323e316f67d09556ecee719541b9
Jesus Ariel Mamani Gutierrez <ariel.10.mg2000@gmail.com>
2025-02-24 22:08:14 -0400

    test: prueba de test con el numero 15 ya definido, proximo commit cuadno haya error, saltos mas grandes

--- src/num_romanos.test.js
+++ src/num_romanos.test.js
@@ -1,4 +1,4 @@
 const { convertirARomano } = require('./num_romanos');
 test('Convertir 10 a romano', () => {
-    expect(convertirARomano(10)).toBe('X');
+    expect(convertirARomano(15)).toBe('XV');
   });
