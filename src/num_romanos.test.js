4803fcc1a694ef0271635fd72d8134763cec921b
Jesus Ariel Mamani Gutierrez <ariel.10.mg2000@gmail.com>
2025-02-24 22:10:27 -0400

    test: prueba de 1 a 50 y 50 no definido

--- src/num_romanos.test.js
+++ src/num_romanos.test.js
@@ -1,4 +1,4 @@
 const { convertirARomano } = require('./num_romanos');
 test('Convertir 10 a romano', () => {
-    expect(convertirARomano(15)).toBe('XV');
+    expect(convertirARomano(50)).toBe('L');
   });
