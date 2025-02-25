f8ea511082c0427f7f2fc7a66d6618f2ffe6b512
Jesus Ariel Mamani Gutierrez <ariel.10.mg2000@gmail.com>
2025-02-24 21:26:37 -0400

    test: La funcion da error ya que que con el 2 devuelve I

--- src/num_romanos.test.js
+++ src/num_romanos.test.js
@@ -3,3 +3,9 @@ const { convertirARomano } = require('./num_romanos');
 test('Convertir 1 a romano', () => {
   expect(convertirARomano(1)).toBe('I');
 });
+
+const { convertirARomano } = require('./num_romanos');
+
+test('Convertir 1 a romano', () => {
+  expect(convertirARomano(2)).toBe('II');
+});
