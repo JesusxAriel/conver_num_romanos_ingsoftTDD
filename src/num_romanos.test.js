1cdf3dce40302d93a325118cd5da93effef51053
Jesus Ariel Mamani Gutierrez <ariel.10.mg2000@gmail.com>
2025-02-24 22:05:51 -0400

    test: prueba de test con el numero 10 no definido

--- src/num_romanos.test.js
+++ src/num_romanos.test.js
@@ -1,4 +1,4 @@
 const { convertirARomano } = require('./num_romanos');
-test('Convertir 9 a romano', () => {
-    expect(convertirARomano(9)).toBe('IX');
+test('Convertir 10 a romano', () => {
+    expect(convertirARomano(10)).toBe('X');
   });
