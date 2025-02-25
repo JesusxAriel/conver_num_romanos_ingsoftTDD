8aa8a53d551d92fcf7e7922e1967966082267943
Jesus Ariel Mamani Gutierrez <ariel.10.mg2000@gmail.com>
2025-02-24 21:41:57 -0400

    add: la funcion ya retorna el valor correcto de 3 a III

--- src/num_romanos.js
+++ src/num_romanos.js
@@ -2,6 +2,7 @@
 function convertirARomano(numero) {
     if (numero === 1) return 'I';
     if (numero === 2) return 'II';
+    if (numero === 3) return 'III';
   }
   
   module.exports = { convertirARomano };
